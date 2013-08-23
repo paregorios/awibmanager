bb56a5b4e86cf450ad526ad0d5ffc4c4af8da51f
Tom Elliott <tom.elliott@nyu.edu>
2013-08-23 05:51:39 -0500

    add name, email, and description to package metadata

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ from setuptools import setup
 setup(
     name='awibmanager',
     version='0.1',
-    author='',
-    author_email='',
+    author='Tom Elliott',
+    author_email='tom.elliott@nyu.edu',
     packages=['awibmanager'],
     url='',
     license='See LICENSE.txt',
-    description='',
+    description='Scripts for managing the Ancient World Image Bank (AWIB)',
     long_description=open('README.txt').read(),
 )
