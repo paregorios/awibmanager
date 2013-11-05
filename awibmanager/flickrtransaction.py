5637315c4165609b23305a0306ceffce060af95b
Tom Elliott <tom.elliott@nyu.edu>
2013-11-05 15:10:08 -0600

    wah

--- awibmanager/flickrtransaction.py
+++ awibmanager/flickrtransaction.py
@@ -1,5 +1,7 @@
 import flickrapi
 import json
+import sys
+from os.path import basename, isfile, splitext
 
 class FlickrTransaction():
 	""" Models a transaction with the Flickr API """
@@ -11,6 +13,7 @@ class FlickrTransaction():
 		self.flickr = None
 		self.key = ''
 		self.private = ''
+		self.uploaded = []
 
 		if key is not None:
 			self.key = key
@@ -20,12 +23,12 @@ class FlickrTransaction():
 			self.authenticate()
 		# note this leaves self.auth == None if auth parameter was False
 
-	def authenticate(self, format='json'):
+	def authenticate(self, format='etree'):
 
 		if self.key!='' and self.secret!='':
 			try:
 				self.flickr = flickrapi.FlickrAPI(self.key, self.secret, format=format)
-				(token, frob) = self.flickr.get_token_part_one(perms='write')
+				(token, frob) = self.flickr.get_token_part_one(perms='delete')
 				if not token: raw_input("Press ENTER after you authorized this program to connect to your Flickr account.")
 				self.flickr.get_token_part_two((token, frob))
 				self.auth = True
@@ -36,6 +39,19 @@ class FlickrTransaction():
 			except:
 				raise
 
+	def delete(self, photoid):
+		self.emsg=''
+		funcn = "photos_delete"
+		try:
+			func = getattr(self.flickr, funcn)
+			resp = func(api_key=self.key, photo_id=photoid)
+			if resp.attrib['stat']=='ok':
+				# remove upload from list
+				self.uploaded = [x for x in self.uploaded if x != photoid]
+			return resp
+		except:
+			raise
+
 	def getList(self, listname):
 
 		self.emsg=''
@@ -43,7 +59,7 @@ class FlickrTransaction():
 		try:
 			func = getattr(self.flickr, funcn)
 			self.rawresp = func(api_key=self.key)
-			return self.__parseResp__()
+			return self.rawresp
 
 		except AttributeError:
 			self.emsg = "FlickrTransaction.getList() could not find a function named %s in the flickrapi package" % funcn
@@ -53,21 +69,33 @@ class FlickrTransaction():
 			self.emsg = err.message
 			return None
 
-	def __parseResp__(self, format='json'):
+
+	def upload(self, filepath, title='', description='', tags='', public=0):
+		""" upload a file to flickr """
 
 		self.emsg = ''
-		self.resp = None
-		result = None
-		if format=='json':
-			if self.rawresp[:14] == "jsonFlickrApi(":
-				jstr = self.rawresp[14:-1]
-				self.resp = json.loads(jstr)
-				if self.resp['stat'] == 'fail':
-					self.emsg = "Flickr API responded with failure message: %s: %s" \
-								% (self.resp['code'], self.resp['message'])
+		if isfile(filepath):
+			fn = basename(filepath)
+			r, e = splitext(fn)
+			if title=='': title=r
+			try:
+				resp = self.flickr.upload(filename=filepath, title=title, description=description, tags=tags, is_public=public)
+				if resp.attrib['stat']=='ok':
+					pid = resp.find('.//photoid').text
+					self.uploaded.append(pid)
+					return pid
 				else:
-					result = self.resp
-		return result
+					print "WHAM: %s" % resp.attrib['stat']
+					return None
+			except flickrapi.FlickrError, err:
+				self.emsg = err.message
+				print "BAM %s" % self.emsg
+				return None
+		else:
+			print "%s is not a valid file" % filepath
+
+
+
 
 
 
